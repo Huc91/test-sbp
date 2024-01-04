c3fc61aac1294622545047b4fa4fdd951f573917
Huc <luca.ucciero@gmail.com>
2024-01-04 16:18:09 +0100

    feat: scaffolding

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,20 @@
+<script setup>
+import WelcomeItem from './WelcomeItem.vue'
+
+const userInput = defineModel('')
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <template #heading>Documentation</template>
+
+    Vue’s
+    <a href="https://vuejs.org/" target="_blank" rel="noopener">official documentation</a>
+    provides you with all information you need to get started.
+  </WelcomeItem>
+  <input v-model="userInput" type="input" />
+  <p>{{ userInput }}</p>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<script async setup>
+import DynamicForm from '../components/DynamicForm.vue'
+
+</script>
+
+<template>
+  <main>
+    <Suspense>
+      <DynamicForm></DynamicForm>
+      <template #fallback>
+        Fetching form data...
+      </template>
+    </Suspense>
+
+  </main>
+</template>
