677fe988b9800415337be0db9af8174e12e31c51
Huc <luca.ucciero@gmail.com>
2024-01-08 15:19:26 +0100

    feat: small test unit

--- src/components/__tests__/HelloWorld.spec.js
+++ src/components/__tests__/HelloWorld.spec.js
@@ -1,11 +1,29 @@
 import { describe, it, expect } from 'vitest'
-
 import { mount } from '@vue/test-utils'
-import HelloWorld from '../HelloWorld.vue'
+import DynamicForm from '../DynamicForm.vue'
 
-describe('HelloWorld', () => {
-    it('renders properly', () => {
-        const wrapper = mount(HelloWorld, { props: { msg: 'Hello Vitest' } })
-        expect(wrapper.text()).toContain('Hello Vitest')
+describe('YourComponent', () => {
+    it('fill the input and submits the form', async () => {
+        const wrapper = mount(DynamicForm, {
+            props: {
+                formSchema: {
+                    fields: [
+                        {
+                            label: 'Full Name',
+                            type: 'text',
+                            model: 'fullName',
+                            placeholder: 'John Doe',
+                            required: true,
+                            minLength: 2,
+                            maxLength: 50,
+                        },
+                    ],
+                },
+                submitText: 'Submit',
+            },
+        })
+        await wrapper.find('input').setValue('test')
+        await wrapper.find('form').trigger('submit.prevent')
+        expect(wrapper.emitted().submit[0][0].fullName).toEqual('test')
     })
 })
